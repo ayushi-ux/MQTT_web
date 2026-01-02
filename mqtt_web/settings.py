fcfd99a2c7f38429bf5455e10793393205df457d
ayushi-ux <jilupatel2004@gmail.com>
2026-01-02 15:26:38 +0530

    Use Railway DATABASE_URL for all services

--- mqtt_web/settings.py
+++ mqtt_web/settings.py
@@ -87,9 +87,8 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgresql://postgres:2004@127.0.0.1:5432/mqtt_django_db",
         conn_max_age=600,
-        ssl_require=False,
+        ssl_require=True,
     )
 }
 

86dfd99d1ac0562c18404bb8d155518ea113345e
ayushi-ux <jilupatel2004@gmail.com>
2026-01-03 11:49:01 +0530

    changes

--- mqtt_web/settings.py
+++ mqtt_web/settings.py
@@ -86,27 +86,15 @@ WSGI_APPLICATION = 'mqtt_web.wsgi.application'
 
 from pathlib import Path
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+import dj_database_url
 
-if DATABASE_URL:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("PGDATABASE"),
-            "USER": os.getenv("PGUSER"),
-            "PASSWORD": os.getenv("PGPASSWORD"),
-            "HOST": os.getenv("PGHOST"),
-            "PORT": os.getenv("PGPORT"),
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
